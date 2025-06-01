94b7cd72ceacaa976bdf2531e9e3482074e4baee
Ndahimana Bonheur <ndahimana154@gmail.com>
2025-06-01 22:38:44 +0800

    Remove the auth for get all categories

--- src/routes/categoryRoutes.ts
+++ src/routes/categoryRoutes.ts
@@ -8,28 +8,28 @@ import categoryController from '../controller/categoryController'
 const CategoryRoute = express.Router()
 
 CategoryRoute.post(
-    "/create-category", userAuthorization(["admin"]),
-    bodyValidation(createCategorySchema), isCategoryAlreadyExist,
-    categoryController.createCategory
- );
+   "/create-category", userAuthorization(["admin"]),
+   bodyValidation(createCategorySchema), isCategoryAlreadyExist,
+   categoryController.createCategory
+);
 
 CategoryRoute.delete(
-    "/delete-category/:id", userAuthorization(["admin"]),
-    isCategoryExistById, categoryController.deleteCategory
- );
+   "/delete-category/:id", userAuthorization(["admin"]),
+   isCategoryExistById, categoryController.deleteCategory
+);
 CategoryRoute.get(
-    "/get-all-categories", userAuthorization(["admin","seller"]),
-    categoryController.getAllCategory
- );
- CategoryRoute.get(
-    "/get-single-category/:id", userAuthorization(["admin"]),
-    categoryController.getSingleCategory
- );
+   "/get-all-categories",
+   categoryController.getAllCategory
+);
+CategoryRoute.get(
+   "/get-single-category/:id", userAuthorization(["admin"]),
+   categoryController.getSingleCategory
+);
 
- CategoryRoute.put(
-    "/update-category/:id", userAuthorization(["admin"]),
-    bodyValidation(updateCategorySchema),isCategoryExistById,
-    isDataProvided,categoryController.updateCategory
+CategoryRoute.put(
+   "/update-category/:id", userAuthorization(["admin"]),
+   bodyValidation(updateCategorySchema), isCategoryExistById,
+   isDataProvided, categoryController.updateCategory
 )
 
 export default CategoryRoute
